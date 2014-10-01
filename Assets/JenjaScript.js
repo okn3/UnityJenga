941b174d172d6806046424191ecc4ece01381d54
okunoryo <okuno@okuno.local>
2014-10-01 11:24:47 +0900

    当たり判定を１部品のみテスト実装

--- Assets/JenjaScript.js
+++ Assets/JenjaScript.js
@@ -7,11 +7,12 @@ function Update () {
 	transform.position.x += 0.1;
 }
 
+//当たり判定
 function OnCollisionEnter(obj:Collision){
 	if (obj.gameObject.name == "Table")
 		Debug.Log("Success! Next!");
 	// }else{
 	// 	Debug.Log("Hit Left");
 	// }
-	// Debug.Log("Hit");
+
 }

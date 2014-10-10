4b624ee04047bc09ebd2fbe817c62a66b0a9f3f2
okunoryo <okuno@okuno.local>
2014-10-11 02:18:43 +0900

    ランキング画面の変更

--- Assets/gameOverScript.js
+++ Assets/gameOverScript.js
@@ -1,26 +1,27 @@
 ﻿
 static var ranking = new ArrayList();
 function Start () {
-	// Invoke("marioSE", 2);
-	// marioSE();
+
   Invoke("restart", 2.5);
 }
 // GUI描画
 var style : GUIStyle;
 function OnGUI() {
-	GUI.contentColor = Color.black;
+	// GUI.contentColor = Color.black;
+	GUI.contentColor = Color.white;
 	var n = 1;
 	ranking.Sort();
 	ranking.Reverse();
 	for (var i = 0; i < (ranking.Count - 1); i++) {
-	  if (ranking[i] == ranking[i+1]) ranking.RemoveAt(i);	
+	  if (ranking[i] == ranking[i+1]) ranking.RemoveAt(i);
 	}
-	GUI.Label(Rect(320,20,300,100), "ランキング", style);
+	// GUI.Label(Rect(320,20,300,100), "ランキング", style);
 	for (score in ranking){
-      GUI.Label(Rect(320,30 + (n * 100),300,100), n + "位： " + score, style);
+      // GUI.Label(Rect(320,330 + (n * 100),300,100), n + "位： " + score, style);
+      GUI.Label(Rect(Screen.width/2-150,360 + (n * 100),300,100), n + "位： " + score, style);
       if (n > 2) break;
       n++;
-	}    
+	}
 }
 
 function restart() {
